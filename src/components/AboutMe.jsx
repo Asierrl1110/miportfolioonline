1d85f659a25af37d3e7da9b8da583e0189ee26bf
Asierrl <asierlamas05@gmail.com>
2026-04-18 10:24:26 +0200

    Component aboutme get the data from the portfolio file

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,23 +1,16 @@
 import React from 'react'
+import { aboutme } from '../data/portfolio'
 
 function AboutMe() {
   return (
     <section id="aboutme" className="aboutme">
         <div className="aboutme-container">
             <h3 className="aboutme-title">Sobre mi</h3>
-            <p className="aboutme-text">
-                Soy desarrollador junior de aplicaciones multiplataforma
-            </p>
-            <p className="aboutme-text">
-                Día a día aprendiendo nuevas tecnologías con el objetivo de construir aplicaciones eficientes, seguras y
-                atractivas
-            </p>
-            <p className="aboutme-text">
-                Recien graduado en DAM y actualmente estudiando DAW, que espero terminarlo en 2027
-            </p>
-            <p className="aboutme-text">
-                Abierto a nuevas oportunidades con el objetivo de seguir creciendo en este sector
-            </p>
+            {aboutme.map((item) => {
+                return (
+                    <p className="aboutme-text">{item}</p>
+                )
+            })}
         </div>
     </section>
   )
