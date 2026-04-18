2286162179c01683ae4c8df4c8c3b53b57d4c087
Asierrl <asierlamas05@gmail.com>
2026-04-18 10:19:25 +0200

    Data file created

---
+++ src/data/portfolio.js
@@ -0,0 +1,161 @@
+import brainboostClient from '../assets/img/brainboostclient.jpg'
+import brainboostServer from '../assets/img/brainboostserver.jpg'
+import cajaRegistradora from '../assets/img/cajaregistradora.jpg'
+
+import java from '../assets/img/java.svg'
+import csharp from '../assets/img/csharp.png'
+import kotlin from '../assets/img/kotlin.png'
+import html from '../assets/img/html5.png'
+import css from '../assets/img/css.png'
+import javascript from '../assets/img/javascript.png'
+import nodejs from '../assets/img/nodejs.png'
+import expressjs from '../assets/img/expressjs.svg'
+import reactt from '../assets/img/react.png'
+import python from '../assets/img/python.png'
+import sqlserver from '../assets/img/sqlserver.png'
+import mongodb from '../assets/img/mongodb.png'
+import git from '../assets/img/git.png'
+import github from '../assets/img/github.png'
+import postman from '../assets/img/postman.png'
+
+export const education = [
+    {
+        "title": "CFGS Desarrollo de Aplicaciones Web",
+        "school": "CIFP La Laboral",
+        "duration": "Sept. 2025 - Actualidad"
+    },
+    {
+        "title": "CFGS Desarrollo de Aplicaciones Multiplataforma",
+        "school": "IES Número 1",
+        "duration": "Sept. 2023 - Jun. 2025"
+    },
+    {
+        "title": "Bachillerato",
+        "school": "IES Montevil",
+        "duration": "Sept. 2021 - Jun. 2023"
+    }
+];
+
+export const experience = [
+    {
+        "title": "Desarrollador ABAP Junior",
+        "company": "Lutech",
+        "duration": "Feb. 2026 - Actualidad",
+        "function-items": [
+            "Desarrollos ABAP con reports, clases, smartforms Y diccionario de datos en sistemas ECC",
+            "Mantenimiento en sistemas ECC",
+            "En continua formación en ABAP, UI5, CAP, RAP y SAP Fiori Elements"
+        ]
+    },
+    {
+        "title": "Desarrollador ABAP Junior",
+        "company": "Capgemini",
+        "duration": "Abr. 2025 - Jun. 2025",
+        "function-items": [
+            "Desarrollo de reports en lenguaje ABAP y ABAP OO",
+            "Trabajo con bases de datos mediante Open SQL",
+            "Visualización de datos mediante ALVs modernos",
+            "Creación de tablas y ayudas de búsqueda en diccionario de datos de un sistema ECC",
+            "Participación en un bootcamp de 3 semanas de formación básica en ABAP, UI5 y BTP"
+        ]
+    }
+];
+
+export const projects = [
+    {
+        "image": brainboostClient,
+        "name": "Brainboost Client",
+        "text": "Brainboost es una aplicación de estudio de flashcards, su objetivo es ayudar a los distintos usuarios de la aplicación para la memorización y aprendizaje de una forma más rápida de conceptos clave.",
+        "tecnologies": [
+            "Kotlin",
+            "XML",
+            "SQLite"
+        ],
+        "url": "https://github.com/Asierrl1110/BrainboostClient"
+    },
+    {
+        "image": brainboostServer,
+        "name": "Brainboost Server",
+        "text": "Brainboost es una aplicación de estudio de flashcards, su objetivo es ayudar a los distintos usuarios de la aplicación para la memorización y aprendizaje de una forma más rápida de conceptos clave.",
+        "tecnologies": [
+            "Java",
+            "JavaSwing",
+            "SQL Server"
+        ],
+        "url": "https://github.com/Asierrl1110/BrainboostServer"
+    },
+    {
+        "image": cajaRegistradora,
+        "name": "Caja Registradora",
+        "text": "Consiste en una simulación de una caja registradora de supermercado, permitiendo la gestión de usuarios y productos, la realización de ventas y la generación de tickets en formato TXT y CSV.",
+        "tecnologies": [
+            "Java",
+            "JavaSwing",
+            "JavaHelp"
+        ],
+        "url": "https://github.com/Asierrl1110/CajaRegistradoraDESIN"
+    }
+]
+
+export const skills = [
+    {
+        "image": java,
+        "name": "Java"
+    },
+    {
+        "image": csharp,
+        "name": "C#"
+    },
+    {
+        "image": kotlin,
+        "name": "Kotlin"
+    },
+    {
+        "image": html,
+        "name": "HTML"
+    },
+    {
+        "image": css,
+        "name": "CSS"
+    },
+    {
+        "image": javascript,
+        "name": "JavaScript"
+    },
+    {
+        "image": nodejs,
+        "name": "Node.js"
+    },
+    {
+        "image": expressjs,
+        "name": "Express.js"
+    },
+    {
+        "image": reactt,
+        "name": "React"
+    },
+    {
+        "image": python,
+        "name": "Python"
+    },
+    {
+        "image": sqlserver,
+        "name": "SQL Server"
+    },
+    {
+        "image": mongodb,
+        "name": "MongoDB"
+    },
+    {
+        "image": git,
+        "name": "Git"
+    },
+    {
+        "image": github,
+        "name": "GitHub"
+    },
+    {
+        "image": postman,
+        "name": "Postman"
+    }
+]
