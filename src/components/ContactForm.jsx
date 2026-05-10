f1b8ce7f2b39d5d8865d42bba716a7c7d4d757bd
Asierrl <asierlamas05@gmail.com>
2026-05-10 09:59:30 +0200

    Feat: Subject field added into ContactForm

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -6,6 +6,7 @@ function ContactForm() {
     const [formData, setFormData] = useState({
         name: "",
         email: "",
+        subject: "",
         message: ""
     })
 
@@ -49,6 +50,11 @@ function ContactForm() {
                     <label>Email <span className='form-required'>*</span></label>
                     <input type="email" name="email" placeholder="Tu Email" required className='form-email'
                                         value={formData.email} onChange={onChange} />
+                </div>
+                <div className='form-group'>
+                    <label>Asunto <span className='form-required'>*</span></label>
+                    <input type="text" name="subject" placeholder="Asunto del correo" required className='form-subject'
+                                        value={formData.subject} onChange={onChange} />
                 </div>
                 <div className='form-group'>
                     <label>Mensaje <span className='form-required'>*</span></label>
