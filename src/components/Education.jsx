ef8ea8167eb0c392ad56c5ab2e9339932b1fee1c
Asier <asierlamas05@gmail.com>
2026-05-20 11:34:51 +0000

    Fix: Education section warning, keys in map function

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -7,9 +7,9 @@ function Education() {
         <section id="education" className="education">
             <h3 className="education-header">Educación</h3>
             <div className="education-container">
-                {educations.map((education) => {
+                {educations.map((education, index) => {
                     return (
-                        <div className="education-card">
+                        <div key={index} className="education-card">
                             <h4 className="education-title">{education.title}</h4>
                             <p className="education-school">{education.school}</p>
                             <p className="education-duration">{education.duration}</p>
