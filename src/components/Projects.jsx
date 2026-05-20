eacb2d0c737901762bdd4c049ea4d078134df7a5
Asier <asierlamas05@gmail.com>
2026-05-20 11:35:04 +0000

    Fix: Projects section warning, keys in map function

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,15 +15,15 @@ function Projects() {
         <section id="projects" className="projects">
             <h3 className="projects-title">Proyectos</h3>
             <div className="projects-container">
-                {projects.map((project) => {
+                {projects.map((project, index) => {
                     return (
-                        <div className="project-card">
+                        <div key={index} className="project-card">
                         <img className="project-image" src={project.image} alt="Imagen proyecto" />
                         <p className="project-name">{project.name}</p>
                         <p className="project-text">{project.text}</p>
                         <ul className="project-tecnologies">
-                            {project['tecnologies'].map((item) => {
-                                return ( <li>{item}</li> )
+                            {project['tecnologies'].map((item, index) => {
+                                return ( <li key={index}>{item}</li> )
                             })}
                         </ul>
                         <button className="project-button" id="btnBrainboostClient" onClick={() => { goTo(project.url) }}>Ver proyecto</button>
