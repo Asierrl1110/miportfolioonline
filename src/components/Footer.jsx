7d8c88e8979926a5b1d2c595cee8c95742184b4f
Asier <asierlamas05@gmail.com>
2026-05-28 11:30:41 +0000

    Fix: Fix function of button "Volver arriba" from "Footer".

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer id="footer" className="footer">
         <p>© 2026 Desarrollado por Asier Rodríguez Lamas. Hecho con React + Vite</p>
-        <a href="#header" className="back-to-top">Volver arriba</a>
+        <a href="#start" className="back-to-top">Volver arriba</a>
     </footer>
   )
 }
